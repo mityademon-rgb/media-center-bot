b5097c78f9981a28d3f794faf74051c9809ad29e
mityademon-rgb <mityademon@gmail.com>
2025-11-04 12:02:03 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+"""
+Настройки бота
+"""
+import os
+
+# Токен бота
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+
+# Ссылки
+LINKS = {
+    'site': 'https://dkmarfino.ru',
+    'youtube': 'https://www.youtube.com/@-m50',
+    'rutube': 'https://rutube.ru/channel/YOUR_CHANNEL',  # ЗАМЕНИ НА СВОЮ
+    'axis': 'https://artmediaskill.ru'
+}
+
+# ID админа (твой ID)
+ADMIN_ID = 397724997  # ЗАМЕНИ НА СВОЙ
