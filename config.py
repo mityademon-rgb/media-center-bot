3eb5a2213b313a8141e901387f01c5bd8f6bdc1d
mityademon-rgb <mityademon@gmail.com>
2025-11-04 12:11:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,15 +4,14 @@
 import os
 
 # Токен бота
-TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN', '8473634161:AAHv_fbBnQ37TboA9LuHCWwgLpjo66daSlA')
+
+# ID админа (твой ID)
+ADMIN_ID = 397724997
 
 # Ссылки
 LINKS = {
-    'site': 'https://dkmarfino.ru',
+    'site': 'https://promo.artmediaskill.ru/',
     'youtube': 'https://www.youtube.com/@-m50',
-    'rutube': 'https://rutube.ru/channel/YOUR_CHANNEL',  # ЗАМЕНИ НА СВОЮ
     'axis': 'https://artmediaskill.ru'
 }
-
-# ID админа (твой ID)
-ADMIN_ID = 397724997  # ЗАМЕНИ НА СВОЙ
